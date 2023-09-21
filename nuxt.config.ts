2db82c006a5586039df7520d97bebfab27579f98
郑耿林 <569751683@qq.com>
2023-09-21 18:04:08 +0800

    ✨ feat(init file and folders): init files、 folders
    1. make files and folders

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt'
+  ],
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '我是来自于描述' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    }
+  },
   devtools: { enabled: true }
 })
