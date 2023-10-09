e7ba10a5f3b42afec282a5ba7058aff47cd90a07
郑耿林 <569751683@qq.com>
2023-10-09 10:25:46 +0800

    ✨ feat(right nav): add jump action to right nav

--- components/mall/FixRightNav.vue
+++ components/mall/FixRightNav.vue
@@ -2,7 +2,7 @@
   <section class="absolute top-8 -right-20 bg-white">
     <ul class="flex flex-col text-sm">
       <li v-for="(item, index) in navList" :key="index" class="border-b border-slate-100 last:border-none">
-        <NuxtLink class="text-stone-600 hover:bg-red-500 hover:cursor-pointer hover:text-white flex flex-col justify-center items-center p-3">
+        <NuxtLink @click="clickNav(item.action)" class="text-stone-600 hover:bg-red-500 hover:cursor-pointer hover:text-white flex flex-col justify-center items-center p-3">
           <Icon v-if="'topIcon' === item.type" :name="item.top" size="1.4em"></Icon>
           <span v-else="'topLabel' === item.type">{{ item.top }}</span>
           <span class="pt-1">{{ item.bottom }}</span>
@@ -13,6 +13,7 @@
 </template>
 
 <script lang="ts" setup>
+const router = useRouter()
 const navList = ref([
   {
     type: 'topLabel',
@@ -30,13 +31,21 @@ const navList = ref([
     type: 'topIcon',
     top: 'eos-icons:service',
     bottom: '客服',
-    action: '#'
+    action: '/feedback'
   },
   {
     type: 'topIcon',
     top: 'material-symbols:feedback-outline',
     bottom: '反馈',
-    action: '#'
+    action: '/online-service'
   }
 ])
+function clickNav(action:string){
+  if(action.indexOf('#') > -1){
+    // scroll to the top of node
+  }else{
+    // router jume
+    router.push(action)
+  }
+}
 </script>
---
+++ pages/feedback/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    意见反馈
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'account'
+})
+</script>
